44795ab085d75e2d4711bede4609924687947ec3
Macho25 <marekpolednik7@gmail.com>
2025-09-04 21:34:22 +0200

    fixed script

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ xargs -a pkglist.txt sudo apt install -y
 sudo wget -O /usr/local/bin/nvim "https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage"
 sudo chmod +x /usr/local/bin/nvim
 
-cd ~/dotfiles stow */
+cd ~/dotfiles
+stow */
 
 source ~/.bashrc
