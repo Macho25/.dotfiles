3bd0d00de15ae5d8898a00176b05b52344f5cce4
Macho25 <marekpolednik7@gmail.com>
2025-09-05 20:11:21 +0200

    Added debug statement into script

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install -y unzip fontconfig dconf-cli tilix
 
 xargs -a pkglist.txt sudo apt install -y
-
+echo "ALL PACKAGES SHOULD BE INSTALLED"
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 
@@ -15,9 +15,11 @@ mkdir -p ~/.poshthemes
 wget -O ~/.poshthemes/blue-owl.omp.json "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip"
 unzip -j ~/.poshthemes/themes.zip "themes/blue-owl.omp.json" -d ~/.poshthemes/
 rm ~/.poshthemes/themes.zip
+echo "Oh My Posh should be installed"
 
-NVIM_VERSION="v0.11.0" sudo wget -O /usr/local/bin/nvim "https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-x86_64.appimage"
+sudo wget -O /usr/local/bin/nvim "https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage"
 sudo chmod +x /usr/local/bin/nvim
+echo "Neovim should be installed"
 
 if [ ! -d ~/.config/nvim ]; then
     git clone https://github.com/LazyVim/starter ~/.config/nvim
@@ -32,12 +34,14 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/0xProto.zi
 unzip 0xProto.zip -d 0xProto
 rm 0xProto.zip
 fc-cache -fv
+echo "Nerdfonts should be installed"
 
 echo 'eval "$(oh-my-posh init bash --config ~/.poshthemes/blue-owl.omp.json)"' >>~/.bashrc
 echo 'export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins/"' >>~/.bashrc
 
 mkdir -p "$HOME/Pictures"
 wget -O "$HOME/Pictures/starry-landscape.jpg" "https://images.wallpapersden.com/image/download/starry-landscape-4k-cool-blue-moon_bW5tbG6UmZqaraWkpJRobWllrWdma2U.jpg"
+echo "Wallpaper should be installed"
 
 if command -v tilix &>/dev/null; then
     BACKGROUND_IMAGE="$HOME/Pictures/starry-landscape.jpg"
