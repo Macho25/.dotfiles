955f8162ef28dbe6e27aafbe04f13efff9493f03
Macho25 <marekpolednik7@gmail.com>
2025-09-06 15:54:33 +0200

    test debugging with less stuff

--- run.sh
+++ run.sh
@@ -15,48 +15,46 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # git clone https://github.com/LazyVim/starter ~/.config/nvim
 # rm -rf ~/.config/nvim/.git
 
-if ! wget -O /usr/local/bin/nvim "https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage"; then
-    echo "⚠️ Failed to download Neovim. Skipping..."
+if ! sudo wget -O /usr/local/bin/nvim "https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage"; then
+    echo " Failed to download Neovim. Skipping..."
 else
     sudo chmod +x /usr/local/bin/nvim
 fi
 
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts
-if ! wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/0xProto.zip; then
-    echo "⚠️ Failed to download font. Skipping font setup..."
+if ! sudo wget -O ~/.local/share/fonts "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/0xProto.zip"; then
+    echo " Failed to download font. Skipping font setup..."
 else
     unzip 0xProto.zip -d 0xProto
     rm 0xProto.zip
     fc-cache -fv
 fi
 
-mkdir -p "$HOME/Picture"
-USE_IMAGE_BACKGROUND=true
-if ! wget -O "$HOME/Picture/starry-landscape.jpg" "https://images.wallpapersden.com/image/download/starry-landscape-4k-cool-blue-moon_bW5tbG6UmZqaraWkpJRobWllrWdma2U.jpg"; then
-    echo "⚠️ Failed to download background image. Skipping image setup..."
-    USE_IMAGE_BACKGROUND=false
-fi
-
-BACKGROUND_IMAGE="$HOME/Picture/starry-landscape.jpg"
-
-PROFILE_ID=$(dconf read /com/gexperts/Tilix/profiles/ | tr -d "[]' " | cut -d, -f1)
-PROFILE_PATH="/com/gexperts/Tilix/profiles/$PROFILE_ID"
-
-if [ "$USE_IMAGE_BACKGROUND" = true ]; then
-    if [ -f "$BACKGROUND_IMAGE" ]; then
-        echo "🖼️ Setting background image: $BACKGROUND_IMAGE"
-        dconf write ${PROFILE_PATH}/background-image "'$BACKGROUND_IMAGE'"
-        dconf write ${PROFILE_PATH}/background-transparency-percent 0
-        dconf write ${PROFILE_PATH}/background-transparency false
-    else
-        echo "⚠️ Background image not found at $BACKGROUND_IMAGE"
-    fi
-else
-    echo "Skipping background image setup"
-fi
+# mkdir -p "$HOME/Picture"
+# USE_IMAGE_BACKGROUND=true
+# if ! wget -O "$HOME/Picture/starry-landscape.jpg" "https://images.wallpapersden.com/image/download/starry-landscape-4k-cool-blue-moon_bW5tbG6UmZqaraWkpJRobWllrWdma2U.jpg"; then
+#     echo " Failed to download background image. Skipping image setup..."
+#     USE_IMAGE_BACKGROUND=false
+# fi
+#
+# BACKGROUND_IMAGE="$HOME/Picture/starry-landscape.jpg"
+#
+# if [ "$USE_IMAGE_BACKGROUND" = true ]; then
+#     PROFILE_ID=$(dconf read /com/gexperts/Tilix/profiles/ | tr -d "[]' " | cut -d, -f1)
+#     PROFILE_PATH="/com/gexperts/Tilix/profiles/$PROFILE_ID"
+#     if [ -f "$BACKGROUND_IMAGE" ]; then
+#         echo " Setting background image: $BACKGROUND_IMAGE"
+#         dconf write ${PROFILE_PATH}/background-image "'$BACKGROUND_IMAGE'"
+#         dconf write ${PROFILE_PATH}/background-transparency-percent 0
+#         dconf write ${PROFILE_PATH}/background-transparency false
+#     else
+#         echo " Background image not found at $BACKGROUND_IMAGE"
+#     fi
+# else
+# echo "Skipping background image setup"
+# fi
 
 # rm -rf ~/.config/tmux ~/.config/nvim
+rm -rf ~/.bashrc
 stow -v tmux nvim bash
 
 tmux source $XDG_CONFIG_HOME/tmux/tmux.conf
